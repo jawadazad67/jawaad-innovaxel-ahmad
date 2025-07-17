2235c39d4e02986f7457a6ae217370aa7993973b
jawadazad67 <jawaadahmadazad67@gmail.com>
2025-07-17 17:50:23 +0500

    accessCount field added in schema model

--- models/Url.js
+++ models/Url.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const UrlSchema = new mongoose.Schema({
     url: { type: String, required: true },
     shortCode: { type: String, required: true, unique: true },
+    accessCount: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
