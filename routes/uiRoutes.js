822b0f261da31951d7b9ac9483a8fc19a09775bd
jawadazad67 <jawaadahmadazad67@gmail.com>
2025-07-17 22:50:12 +0500

    final change in uI/Commentts added

--- routes/uiRoutes.js
+++ routes/uiRoutes.js
@@ -5,11 +5,10 @@ const uiController = require('../controllers/uiController');
 // UI homepage
 router.get('/', uiController.renderHome);
 
-// Minimal UI routes
 router.post('/shorten-ui', uiController.handleShortenUi);
 router.post('/update-ui', uiController.handleUpdateUi);
 router.post('/delete-ui', uiController.handleDeleteUi);
 router.post('/stats-ui', uiController.handleStatsUi);
-router.post('/retrieve-ui', uiController.handleRetrieveUi); // ✅ NEW: Retrieve original URL
+router.post('/retrieve-ui', uiController.handleRetrieveUi);
 
 module.exports = router;
